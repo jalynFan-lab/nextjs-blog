0c1166a55cc0101f40b8b57ed685e3f4909c52c7
kyo.han <kyo.han@refact.co.jp>
2022-06-10 18:02:46 +0900

    delete date-fns

--- components/date.js
+++ components/date.js
@@ -1,6 +1,6 @@
-import { parseISO, format } from "date-fns";
+import dateFormat, { masks } from "dateformat";
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+  const date = dateFormat(dateString, "mmmm dS, yyyy");
+  return <time dateTime={dateString}>{date}</time>;
 }
